d9bebec34e0cb5b74f07e93b7bf462ffe5936dbf
Bruno Wierzbicki <brunowfx@gmail.com>
2024-06-09 20:18:56 -0300

    first commit.

---
+++ uc11.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS uc11 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+
+USE uc11;
+
+CREATE TABLE `produtos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nome` text DEFAULT NULL,
+  `valor` int(11) DEFAULT NULL,
+  `status` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `valor`, `status`) VALUES
+(2, 'PS4', 1500, 'Vendido'),
+(3, 'Xbox 360', 800, 'Vendido'),
+(4, 'Iphone 12', 4800, 'Vendido'),
+(5, 'PS2', 400, 'A Venda');
+
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+SELECT * FROM produtos;
